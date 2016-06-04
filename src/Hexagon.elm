4b2b61510d836736aea016814a14562866dc28cd
Marco <macrozone@gmail.com>
2016-06-04 16:36:50 +0200

    update

--- src/Hexagon.elm
+++ src/Hexagon.elm
@@ -25,7 +25,8 @@ type alias Game =
   , state : State
   , progress : Int
   , timeStart : Time
-  , timeRunning : Time
+  , timeTick : Time
+  , msRunning : Float
   }
 
 (gameWidth, gameHeight) = (1024, 576) -- 16:9
@@ -44,7 +45,8 @@ defaultGame =
   , state = NewGame
   , progress = 0
   , timeStart = 0.0
-  , timeRunning = 0.0
+  , timeTick = 0.0
+  , msRunning = 0.0
   }
 
 -- UPDATE
@@ -75,12 +77,12 @@ updateProgress {state,progress} =
     Play -> progress + 1
     _ -> progress
 
-updateTimeRunning: Time -> Game -> Time
-updateTimeRunning timestamp game = 
+updateMsRunning: Time -> Game -> Time
+updateMsRunning timestamp game = 
   case game.state of
-    Play -> (timestamp - game.timeStart)
-    GameOver -> game.timeRunning
-    _ -> 0.0
+    Play -> game.msRunning + timestamp - game.timeTick 
+    NewGame -> 0.0
+    _ -> game.msRunning
 
 updatePlayer: Input -> Game -> Player
 updatePlayer {dir} {player} =
@@ -98,7 +100,8 @@ update (timestamp, input) game =
     , state =  Debug.watch "state" (updateState input game)
     , progress = Debug.watch "progress" (updateProgress game)
     , timeStart = Debug.watch "timeStart" (if game.state == NewGame then timestamp else game.timeStart)
-    , timeRunning = Debug.watch "timeRunning" (updateTimeRunning timestamp game)
+    , timeTick = timestamp
+    , msRunning = Debug.watch "msRunning" (updateMsRunning timestamp game)
   }
 
 -- VIEW
