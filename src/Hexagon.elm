c0db93ca668d8e80c6dec4f0088ac83f9a2352aa
Simon Bächler <simon@stellanera.com>
2016-06-12 16:36:46 +0200

    first part working

--- src/Hexagon.elm
+++ src/Hexagon.elm
@@ -1,25 +1,30 @@
-module Game where
 import Time exposing ( .. )
 import AnimationFrame
-import Keyboard
+import Keyboard.Extra as Keyboard
 import Window
-import Graphics.Collage exposing (..)
-import Graphics.Element exposing (..)
+import Collage exposing (..)
+import Element exposing (..)
 import Color exposing (..)
+import Html
+import Html.App as App
 
 -- MODEL
 
 type alias Player =
   { angle: Float }
 
-type alias Input =
-  { space : Bool
-  , dir : Int
-  }
+
+type Msg
+  = Init Time
+  | Step Time
+  | Pause
+  | KeyboardExtraMsg Keyboard.Msg
 
 type alias Game =
-  { 
-    player : Player
+  {
+    player : Player,
+    direction : Int,
+    keyboardModel : Keyboard.Model
   }
 
 (gameWidth, gameHeight) = (1024, 576) -- 16:9
@@ -29,17 +34,11 @@ type alias Game =
 playerRadius : Float
 playerRadius = gameWidth / 10.0
 
--- The global game state
 
-defaultGame : Game
-defaultGame =
-  { 
-    player = Player (degrees 30)
-  }
 
 -- UPDATE
 
-updatePlayerAngle: Float -> Int -> Float
+updatePlayerAngle : Float -> Int -> Float
 updatePlayerAngle angle dir =
   let
     newAngle = (angle + toFloat (-dir * 4) * 0.032)
@@ -51,20 +50,30 @@ updatePlayerAngle angle dir =
     else
       newAngle
 
-updatePlayer: Input -> Game -> Player
-updatePlayer {dir} {player} =
+updatePlayer: Int -> Game -> Player
+updatePlayer dir {player} =
   let
     newAngle = updatePlayerAngle player.angle dir
   in
     { player | angle = newAngle }
 
 -- Game loop: Transition from one state to the next.
-update : (Time, Input) -> Game -> Game
-update (timestamp, input) game =
- 
-  { game |
-      player = updatePlayer input game
-  }
+update : Msg -> Game -> (Game, Cmd Msg)
+update msg game =
+  case msg of
+    KeyboardExtraMsg keyMsg ->
+      let
+        ( keyboardModel, keyboardCmd ) =
+          Keyboard.update keyMsg game.keyboardModel
+      in
+        ( { game | keyboardModel = keyboardModel
+                 , direction = (Keyboard.arrows keyboardModel).x
+          }
+        , Cmd.map KeyboardExtraMsg keyboardCmd
+        )
+    Step time -> ({game | player = updatePlayer game.direction game }, Cmd.none)
+    _ -> (game, Cmd.none)
+
 
 -- VIEW
 
@@ -78,7 +87,7 @@ moveRadial angle radius =
 
 makePlayer : Player -> Form
 makePlayer player =
-  let 
+  let
     angle = player.angle - degrees 30
   in
     ngon 3 10
@@ -86,33 +95,52 @@ makePlayer player =
       |> moveRadial angle (playerRadius - 10)
       |> rotate angle
 
-view : (Int,Int) -> Game -> Element
-view (w, h) game =
-  container w h middle <|
+view : Game -> Html.Html Msg
+view game =
+  toHtml <|
+  container gameWidth gameHeight middle <|
   collage gameWidth gameHeight
     [ rect gameWidth gameHeight
         |> filled bgBlack
     , makePlayer game.player
     ]
 
--- SIGNALS
 
-main : Signal Element
+-- SUBSCRIPTIONS
+
+step : Time -> Msg
+step time =
+  Step time
+
+
+subscriptions : Game -> Sub Msg
+subscriptions game =
+  Sub.batch [
+    AnimationFrame.times step,
+    Sub.map KeyboardExtraMsg Keyboard.subscriptions
+  ]
+
+
+
+--INIT
+
+init : (Game, Cmd Msg)
+init =
+  let
+    ( keyboardModel, keyboardCmd ) = Keyboard.init
+  in
+    ( { player = Player (degrees 30),
+        keyboardModel = keyboardModel,
+        direction = 0
+      },
+      Cmd.map KeyboardExtraMsg keyboardCmd
+    )
+
+
+
 main =
-  Signal.map2 view Window.dimensions gameState
-
-gameState : Signal Game
-gameState =
-  Signal.foldp update defaultGame input
-
--- Creates an event stream from the keyboard inputs and is
--- updated by AnimationFrame.
-input : Signal (Time, Input)
-input =
-  Signal.map2 Input
-    Keyboard.space
-    (Signal.map .x Keyboard.arrows)
-  -- only update on a new frame
-  |> Signal.sampleOn AnimationFrame.frame
-  |> Time.timestamp
-
+  App.program
+  { init = init
+  , update = update
+  , view = view
+  , subscriptions = subscriptions }
