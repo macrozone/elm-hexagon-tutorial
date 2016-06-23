5a4a6df4be40621b9d4f6d003f3c293789391d23
Simon Bächler <simon@stellanera.com>
2016-06-23 15:59:32 +0200

    move bgBlack definition to Model section

--- src/Hexagon.elm
+++ src/Hexagon.elm
@@ -35,6 +35,10 @@ type Msg
 playerRadius : Float
 playerRadius = gameWidth / 10.0
 
+bgBlack : Color
+bgBlack =
+  rgb 20 20 20
+
 
 -- UPDATE
 
@@ -98,10 +102,6 @@ update msg game =
 
 
 -- VIEW
-
-bgBlack : Color
-bgBlack =
-  rgb 20 20 20
 
 moveRadial : Float -> Float -> Form -> Form
 moveRadial angle radius =
